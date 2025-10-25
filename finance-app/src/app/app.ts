cd0e497b4821ff15c7236bb886dc3edfedccbf35
Gabrielle Souza <gabisouza234ra@gmail.com>
2025-10-24 22:00:59 -0300

    chore(module): import FormsModule and declare components

--- finance-app/src/app/app.ts
+++ finance-app/src/app/app.ts
@@ -1,12 +1,24 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
-@Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+import { AppRoutingModule } from './app.routes';
+import { App } from './app';
+import { Login } from './pages/login/login';
+import { Home } from './pages/home/home';
+
+@NgModule({
+  declarations: [
+    App,
+    Login,
+    Home
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [App]
 })
-export class App {
-  protected readonly title = signal('finance-app');
-}
+export class AppModule { }
