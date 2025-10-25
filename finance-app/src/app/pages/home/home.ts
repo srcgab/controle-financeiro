98625b9dc8208624223e77f41f52d3a70a732a85
Gabrielle Souza <gabisouza234ra@gmail.com>
2025-10-24 22:13:06 -0300

    feat(home): prototipo de home com mock listagem

--- finance-app/src/app/pages/home/home.ts
+++ finance-app/src/app/pages/home/home.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth';
 
 @Component({
   selector: 'app-home',
-  imports: [],
-  templateUrl: './home.html',
-  styleUrl: './home.css',
+  templateUrl: './home.html'
 })
 export class Home {
+  userName = '';
 
+  constructor(private auth: AuthService) {
+    const u = this.auth.getCurrentUser();
+    this.userName = u ? u.name : 'Usuário';
+  }
 }
