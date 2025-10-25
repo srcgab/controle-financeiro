046803fdf177d64da7c8a1ea70381d6748f00add
Gabrielle Souza <gabisouza234ra@gmail.com>
2025-10-24 23:13:31 -0300

    feat(transactions): add transactions page mock

--- finance-app/src/app/app.routes.ts
+++ finance-app/src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Login } from './pages/login/login';
 import { Home } from './pages/home/home';
+import { Transactions } from './pages/transactions/transactions';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: Login },
   { path: 'home', component: Home},
+  { path: 'transactions', component: Transactions },
   { path: '**', redirectTo: 'login' }
 ];
 
