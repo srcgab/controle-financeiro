0e45c58c48c404e8ae4d84b92f1c7801b536d4b9
Gabrielle Souza <gabisouza234ra@gmail.com>
2025-10-24 21:53:24 -0300

    feat(routing): define routes for login and home

--- finance-app/src/app/app.routes.ts
+++ finance-app/src/app/app.routes.ts
@@ -1,3 +1,17 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { Login } from './pages/login/login';
+import { Home } from './pages/home/home';
 
-export const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: Login },
+  { path: 'home', component: Home},
+  { path: '**', redirectTo: 'login' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
